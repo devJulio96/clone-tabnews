446eef7d618691dcd03b250f909d080f064f996c
Júlio Freitas <julio.dev.freitas@gmail.com>
2024-11-28 17:52:15 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Clóvis de Barros Filho - <a href="https://youtu.be/TRPBY_lxJfE?si=ASH8QOb_4VmnWjNS">Brio</a></h1>
+    return <h1>Clóvis de Barros Filho 😎 - <a href="https://youtu.be/TRPBY_lxJfE?si=ASH8QOb_4VmnWjNS">Brio</a></h1>
 }
 
 export default Home;
