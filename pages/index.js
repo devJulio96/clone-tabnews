61a46d6e75011112446bbb8f1967005ae914b072
Júlio Freitas <julio.dev.freitas@gmail.com>
2024-11-29 18:10:52 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home (){
-    return <h1>Clóvis de Barros Filho 😎 - <a href="https://youtu.be/TRPBY_lxJfE?si=ASH8QOb_4VmnWjNS">Brio</a></h1>
+function Home() {
+  return (
+    <h1>
+      Clóvis de Barros Filho 😎 -{" "}
+      <a href="https://youtu.be/TRPBY_lxJfE?si=ASH8QOb_4VmnWjNS">Brio</a>
+    </h1>
+  );
 }
 
 export default Home;
