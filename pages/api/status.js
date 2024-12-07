7d52c4d6eebac4ea3dd0df552c417427ee70f777
Júlio Freitas <81449489+devJulio96@users.noreply.github.com>
2024-12-07 14:37:34 +0000

    Modifica o arquivo `status.js`

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,3 +1,6 @@
 function status(request, response) {
-  response.status(200).send("Sou aluno do curso.dev");
+  // response.status(200).send("Sou aluno do curso.dev");
+  response.status(200).json({ chave: "sou aluno do curso.dev" });
 }
+
+export default status;
